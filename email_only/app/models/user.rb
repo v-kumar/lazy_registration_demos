00c3df4dae984d425d1307abaac56c0a0d51bbc5
Vikram Kumar <vikram.kumar@gonift.com>
2018-06-02 15:48:50 -0400

    support multi email

--- email_only/app/models/user.rb
+++ email_only/app/models/user.rb
@@ -2,29 +2,33 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  
-  devise :confirmable, :lockable
+  has_many :emails
+
+  devise :recoverable, :rememberable, :trackable, :registerable
+
+  # You should not declare :confirmable and :multi_email_confirmable at the same time.
+  devise :multi_email_authenticatable, :multi_email_validatable, :multi_email_confirmable
+
+  devise :lockable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+
   def password_required?
     # Password is required if it is being set, but not for new records
-    if !persisted? 
+    if !persisted?
       false
     else
       !password.nil? || !password_confirmation.nil?
     end
   end
-  
+
   # provide access to protected method unless_confirmed
   def only_if_unconfirmed
     pending_any_confirmation {yield}
   end
-  
-  # set the password without knowing the current password used in our confirmation controller. 
+
+  # set the password without knowing the current password used in our confirmation controller.
   def update_password(params)
     update_attributes({password: params[:password], password_confirmation: params[:password_confirmation]})
   end
