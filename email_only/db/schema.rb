c8893d9b1c27e91628fb7d3ae60caf3d97dd63dd
Vikram Kumar <vikram.kumar@gonift.com>
2018-06-02 15:49:43 -0400

    updated schema

--- email_only/db/schema.rb
+++ email_only/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131029144133) do
+ActiveRecord::Schema.define(version: 20180602154452) do
+
+  create_table "emails", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "email"
+    t.boolean  "primary",              default: false
+    t.string   "unconfirmed_email"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+  end
 
   create_table "pages", force: :cascade do |t|
     t.datetime "created_at"
@@ -19,7 +29,6 @@ ActiveRecord::Schema.define(version: 20131029144133) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -29,10 +38,6 @@ ActiveRecord::Schema.define(version: 20131029144133) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.integer  "failed_attempts",        default: 0
     t.string   "unlock_token"
     t.datetime "locked_at"
@@ -44,8 +49,6 @@ ActiveRecord::Schema.define(version: 20131029144133) do
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
 
